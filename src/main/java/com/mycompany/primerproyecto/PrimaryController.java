a01b682be6b843a46449f80891379b38dd6fd825
CMaster <CMaster@DESKTOP-K2CQ5G0>
2021-05-03 19:50:52 +0200

    nuevoooo

--- src/main/java/com/mycompany/primerproyecto/PrimaryController.java
+++ src/main/java/com/mycompany/primerproyecto/PrimaryController.java
@@ -5,7 +5,6 @@ import com.mycompany.entidades.Usuario;
 import java.io.IOException;
 import java.sql.SQLException;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -16,8 +15,6 @@ public class PrimaryController {
     @FXML
     private TextField name;
     @FXML
-    private Label resusuario;
-    @FXML
     private PasswordField pass;
 
     @FXML
@@ -27,25 +24,6 @@ public class PrimaryController {
     @FXML
     private ImageView imagen;
 
-    @FXML
-    private void verificar() {
-        String usuario = name.getText();
-        String password = pass.getText();
-        Usuario u = new Usuario();
-        boolean comp = u.nombreUsuario(usuario);
-        boolean comp2 = u.passwordtrue(password);
-
-        if (comp) {
-            resusuario.setText("Inicio de sesion correcto");
-            if (comp2) {
-                resusuario.setText("Contraseña correcta");
-            } else {
-                resusuario.setText("La contraseña es incorrecta");
-            }
-        } else {
-            resusuario.setText("El nombre de usuario es incorrecto");
-        }
-    }
 
     public void loadImage() {
         Image img = new Image(getClass().getResourceAsStream("/images/cliente3.jpg"));
